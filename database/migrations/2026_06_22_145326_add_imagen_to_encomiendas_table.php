bf08f18780f7950a32a719804734284108fdbb6b
FabrizioSuasaca <suasacafabrizio@gmail.com>
2026-06-22 09:54:14 -0500

    codigo migracion imagen encomiendas

--- database/migrations/2026_06_22_145326_add_imagen_to_encomiendas_table.php
+++ database/migrations/2026_06_22_145326_add_imagen_to_encomiendas_table.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('encomiendas', function (Blueprint $table) {
-            //
+            $table->string('imagen')->nullable()->after('descripcion');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('encomiendas', function (Blueprint $table) {
-            //
+            $table->dropColumn('imagen');
         });
     }
 };
